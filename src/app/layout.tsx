b34d5292a8fb710530304c3cf22bfb1ea9d9982b
omr-9 <omaralsharkawi9@gmail.com>
2025-01-23 17:38:20 +0200

    add header and clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Header from "@/components/Header";
+import ThemeComponent from "@/components/ThemeComponent";
+import { ClerkProvider } from "@clerk/nextjs";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,12 +25,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeComponent>
+            <Header />
+            {children}
+          </ThemeComponent>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import DarkModeSwitch from './DarkModeSwitch';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+      <ul className='flex gap-4'>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <Link href={'/sign-in'} className='hover:text-amber-300 duration-150
+'>Sign in</Link>
+        </SignedOut>
+        <li className='hidden hover:text-amber-300 duration-150 sm:block'>
+          <Link href={'/'}>Home</Link>
+        </li>
+        <li className='hidden hover:text-amber-300 duration-150 sm:block'>
+          <Link href={'/favorites'}>Favorites</Link>
+        </li>
+        <li className='hidden hover:text-amber-300 duration-150 sm:block'>
+          <Link href={'/about'}>About</Link>
+        </li>
+      </ul>
+      <div className='flex items-center gap-4'>
+        <DarkModeSwitch />
+        <Link href={'/'} className='flex gap-1 items-center'>
+          <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>
+            IMDb
+          </span>
+          <span className='text-xl hidden sm:inline'>Clone</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
